9fd72356b93ca1f8735fb4c8d4c8a77c8f8939a7
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-05-03 16:33:58 +0530

    added keys

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StripeApp from './src/StripeApp';
+import { StripeProvider } from '@stripe/stripe-react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <StripeProvider>
+      <StripeApp />
+    </StripeProvider>
   );
 }
 
---
+++ src/StripeApp.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const StripeApp = () => {
+    return (
+        <View>
+            <Text>StripeApp</Text>
+        </View>
+    )
+}
+
+export default StripeApp
+
+const styles = StyleSheet.create({})
